fa54bd26d944d5ddb5738fd23b35ff521ebd6430
ys <youngsik0266@gmail.com>
2024-01-09 18:22:48 +0900

    [inflearn-react-1] 순수 JS 폴더 구조

---
+++ inflearn-react-1/1-vanila/js/main.js
@@ -0,0 +1,16 @@
+import Controller from "./Controller.js";
+import Store from "./Store.js";
+import storage from "./storage.js";
+
+document.addEventListener("DOMContentLoaded", main);
+
+function main() {
+    const store = new Store(storage);
+
+    const views = {
+        // TODO
+
+    };
+
+    new Controller(store, views);
+}
