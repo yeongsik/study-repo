f35f70cd009205d0e58e6cd34c12d19b043296fb
ys <youngsik0266@gmail.com>
2024-01-10 11:20:42 +0900

    [inflearn-react-1] 순수 JS 검색폼2

--- inflearn-react-1/1-vanila/js/views/SearchFormView.js
+++ inflearn-react-1/1-vanila/js/views/SearchFormView.js
@@ -1,17 +1,29 @@
 import View from "./View.js";
-import {qs} from "../helpers.js";
+import {on, qs} from "../helpers.js";
 
 export default class SearchFormView extends View {
 
     constructor() {
         super(qs("#search-form-view"));
 
+        this.inputElement = qs("[type=text]", this.element);
         this.resetElement = qs("[type=reset]", this.element);
+
+
         this.showResetButton(false);
+        this.bindEvent();
     }
 
     showResetButton(visible = true) {
         this.resetElement.style.display = visible ? "block" : "none";
     }
 
+    bindEvent() {
+        on(this.inputElement, "keyup", () => this.handleKeyup());
+    }
+
+    handleKeyup() {
+        const {value} = this.inputElement;
+        this.showResetButton(value.length > 0);
+    }
 }
