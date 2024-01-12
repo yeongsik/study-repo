bd3c871124824821ec7982c968888d9fefeb298c
ys <youngsik0266@gmail.com>
2024-01-12 11:47:45 +0900

    [inflearn-react-1] 헤더 구현

--- inflearn-react-1/2-react/js/main.js
+++ inflearn-react-1/2-react/js/main.js
@@ -1,5 +1,12 @@
 // 자바스크립트 코드의 시작점
 // 이후에 모듈별로 만들어서 추가할 예정
 
-const element = <h1>Hello world</h1>;
+
+// JSX의 속성은 카멜케이스로 사용한다.
+// class를 사용할 수 없는 이유는 자바스크립트에서 사용하는 예약어 이기 때문에 jsx에서는 className을 사용한다.
+const element = (
+    <header>
+        <h2 className="container">검색</h2>
+    </header>
+);
 ReactDOM.render(element, document.querySelector("#app"));
