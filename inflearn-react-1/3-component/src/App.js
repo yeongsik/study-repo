90ee2b247c1a3ce11b36c72519cd41a8ecd8b2e1
ys <youngsik0266@gmail.com>
2024-01-24 18:22:02 +0900

    [inflearn-react-1] Tabs

--- inflearn-react-1/3-component/src/App.js
+++ inflearn-react-1/3-component/src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import SearchForm from "./components/SearchForm";
 import SearchResult from "./components/SearchResult";
 import store from "./Store.js"
+import Tabs, {TabType} from "./components/Tabs";
 
 export default class App extends React.Component {
     constructor() {
@@ -12,7 +13,7 @@ export default class App extends React.Component {
             searchKeyword: "",
             searchResult: [],
             submitted: false,
-
+            selectedTab: TabType.KEYWORD,
         };
     }
 
@@ -41,7 +42,7 @@ export default class App extends React.Component {
 
 
     render() {
-        const {searchKeyword, submitted, searchResult} = this.state;
+        const {searchKeyword, submitted, searchResult, selectedTab} = this.state;
 
         return (
             <>
@@ -54,7 +55,16 @@ export default class App extends React.Component {
                         onReset={() => this.handleReset()}
                     />
                     <div className="content">
-                        {submitted && <SearchResult data={searchResult} />}
+                        {submitted ? (
+                            <SearchResult data={searchResult} />
+                        ) : (
+                            <>
+                                <Tabs selectedTab={selectedTab} onChange={(selectedTab) => this.setState({selectedTab})} />
+                                {selectedTab === TabType.KEYWORD && <>TODO:추천검색어 목록 </>}
+                                {selectedTab === TabType.HISTORY && <>TODO:최근검색어 목록 </>}
+                            </>
+
+                        )}
                     </div>
                 </div>
             </>
